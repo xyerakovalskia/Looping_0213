8bdb168f45fad88a9f680075282956abf9d02a82
xyerakovalskia <xyera123987@gmail.com>
2026-03-30 20:09:29 +0700

    looping9

--- looping.cpp
+++ looping.cpp
@@ -76,3 +76,11 @@ int main() {
     cin>>nilB;
 
     rerata = (nilB+nilM)/2;
+
+    if ((rerata >= 60) || (nilM >= 70))
+        status = "Lulus";
+    else
+        status = "Tidak Lulus";
+
+    cout<<"Status kelulusan = "<<status<< ",dengan nilai rata rata = "<<rerata<<endl;
+}
