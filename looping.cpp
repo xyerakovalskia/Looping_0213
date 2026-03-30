25ce1970c4dadfb1566e4704c717a38755dc81ea
xyerakovalskia <xyera123987@gmail.com>
2026-03-30 20:10:50 +0700

    looping11

--- looping.cpp
+++ looping.cpp
@@ -92,3 +92,10 @@ using namespace std;
 int main() {
     float nilB,nilM,rerata;
     string status;
+
+    cout<<"Masukkan nilai Matematika = ";
+    cin>>nilM;
+    cout<<"Masukkan nilai Bahasa Inggris = ";
+    cin>>nilB;
+
+    rerata = (nilB+nilM)/2;
