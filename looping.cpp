dbb964466cbb0eb268bd21f5cd4688dc9b9ff7b6
xyerakovalskia <xyera123987@gmail.com>
2026-03-30 20:02:02 +0700

    looping1

---
+++ looping.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <ctime>
+using namespace std;
+
+int main() {
+    int x;
+
+    //Perulangan dengan FOR
+    //mencetak tulisan sebanyak 3 kali
+    cout<< "PERULANGAN FOR"<<endl;
+    for(int i=0; i<3; i++){
+        cout<< "Teknologi Informasi UMY"<<endl;
+    }
